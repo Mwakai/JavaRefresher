e3ae664ca6df8b6dff7c9ddef03b379dc7a0e085
Alvin Mwakai <mwakaimwambala@gmail.com>
2023-08-30 16:39:12 +0300

    challenge five

---
+++ src/com/mwakai/hackerrank/OutputFormatting.java
@@ -0,0 +1,23 @@
+package com.mwakai.hackerrank;
+
+import java.util.Scanner;
+
+public class OutputFormatting {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("================================");
+
+        for(int i=0;i<3;i++){
+            String s1=scanner.next();
+            int x=scanner.nextInt();
+            //Complete this line
+            System.out.printf("%-15s%03d%n", s1, x);
+
+        }
+        System.out.println("================================");
+
+        scanner.close();
+    }
+}
