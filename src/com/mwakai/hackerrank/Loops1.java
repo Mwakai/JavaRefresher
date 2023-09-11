d0a06594b51a7b447faa5c12ef846cad5393ccea
Alvin Mwakai <mwakaimwambala@gmail.com>
2023-09-11 14:50:43 +0300

    loops

---
+++ src/com/mwakai/hackerrank/Loops1.java
@@ -0,0 +1,18 @@
+package com.mwakai.hackerrank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Loops1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+
+    }
+}
